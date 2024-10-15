b57908e083812cbd60e2b24ce0305f051bd0b2a7
NettLN <161901545+NettLN@users.noreply.github.com>
2024-10-14 21:48:24 -0400

    Practica Segundo Parcial

--- listaS/LISTA.py
+++ listaS/LISTA.py
@@ -40,9 +40,18 @@ class Lista:
             actual.siguiente = nuevo_nodo
     
     def eliminar_inicio(self):
-        # aux = self.P
-        
-        
+        if self.P == None:
+            print("Lista Vacia")
+            
+        elif self.P.siguiente is None:
+            print("Se ha eliminado:", self.P)
+            self.P = None
+            
+        else:
+            print("Se ha eliminado del inicio:", self.P.elemento)
+            actual = self.P
+            self.p = actual.siguiente
+            
         if self.P is not None:
             self.P = self.P.siguiente
         # return aux
@@ -52,6 +61,7 @@ class Lista:
             print("Lista Vacia")
             
         elif self.P.siguiente is None:
+            print("Se ha eliminado:", self.P)
             self.P = None
             
         else:
@@ -59,6 +69,7 @@ class Lista:
             while actual.siguiente:
                 anterior = actual
                 actual = actual.siguiente
+            print("Se ha eliminado del final:", actual.elemento)
             anterior.siguiente = None
 
 # def valores_iguales(self, lista1, lista2):
@@ -76,26 +87,143 @@ class Lista:
 #                 actual2 = actual2.siguiente
 #                 else:
 #                     print("No hay elemento iguales")
-                    
-            
+
+def eliminar2(lista):
+    if lista.es_vacia():
+        print("La lista esta vacia")
+    
+    else:
+        actual = lista.P
+        while lista.P.elemento % 2 == 0 and actual.siguiente != lista.P:
+            print("Se ha eliminado:", lista.P.elemento)
+            lista.P = lista.P.siguiente
+
+        actual = lista.P            
+        while actual.siguiente != None:
+            if actual.siguiente.elemento % 2 == 0:
+                print("Se ha eliminado:", actual.siguiente.elemento)
+                actual.siguiente =  actual.siguiente.siguiente
+            else:
+                actual = actual.siguiente
                 
-                    
-            
+def comparar(lista1, lista2):
+    if lista1.es_vacia() and lista2.es_vacia():
+        print("Ambas listas están vacías")
+        return
+    elif lista1.es_vacia():
+        print("La lista 1 está vacía")
+        lista2.mostrar_lista()
+        return
+    elif lista2.es_vacia():
+        print("La lista 2 está vacía")
+        lista1.mostrar_lista()
+        return
+
+    actual1 = lista1.P
+    max1 = actual1.elemento
+    while actual1:
+        if actual1.elemento > max1:
+            max1 = actual1.elemento
+        actual1 = actual1.siguiente
+        
+    actual2 = lista2.P
+    max2 = actual2.elemento
+    while actual2:
+        if actual2.elemento > max2:
+            max2 = actual2.elemento
+        actual2 = actual2.siguiente
+        
+    if max1 > max2:
+        print(f"El mayor elemento {max1} es de la lista:")
+        lista1.mostrar_lista()
+    
+    elif max2 > max1:
+        print(f"El mayor elemento {max2} es de la lista:")
+        lista2.mostrar_lista()
+    
+def juntar_listas(lista1, lista2):
+    if lista1.es_vacia() and lista2.es_vacia():
+        print("Ambas listas están vacías")
+        return
+    elif lista1.es_vacia():
+        print("La lista 1 está vacía")
+        lista2.mostrar_lista()
+        return
+    elif lista2.es_vacia():
+        print("La lista 2 está vacía")
+        lista1.mostrar_lista()
+        return
+    
+    actual = lista1.P
+    while actual.siguiente:
+        actual = actual.siguiente
+        
+    actual.siguiente = lista2.P
+    
+    lista1.mostrar_lista()
+    return lista1
+
+def ordenar_lista(lista):
+    if lista.es_vacia() or lista.P.siguiente is None:
+        return 
+    
+    intercambiado = True
+    while intercambiado:
+        actual = lista.P
+        intercambiado = False
+        
+        while actual.siguiente:
+            if actual.elemento > actual.siguiente.elemento:
+                actual.elemento, actual.siguiente.elemento = actual.siguiente.elemento, actual.elemento
+                intercambiado = True
+            actual = actual.siguiente
+           
 # prueba = Lista()
 # prueba.agregar_inicio(2)
 # prueba.agregar_inicio(65)
-
 # prueba.mostrar_lista()
-
 # prueba.agregar_final(43)
 # prueba.agregar_final(56)
-
 # prueba.mostrar_lista()
-
 # print(str(prueba.contar_nodos()))
-
 # prueba.eliminar_final()
 # prueba.mostrar_lista()
-
 # prueba.eliminar_inicio()
 # prueba.mostrar_lista()
+
+# lista_numeros = Lista()
+# lista_numeros.agregar_final(6)
+# lista_numeros.agregar_final(6)
+# lista_numeros.agregar_final(9)
+# lista_numeros.agregar_final(12)
+# lista_numeros.agregar_final(15)
+# lista_numeros.agregar_final(21)
+# lista_numeros.agregar_final(30)
+# lista_numeros.mostrar_lista()
+
+# eliminar2(lista_numeros)
+# lista_numeros.mostrar_lista()
+
+listaA = Lista()
+listaA.agregar_final("a")
+listaA.agregar_final("Z")
+listaA.agregar_final("z")
+listaA.agregar_final("h")
+
+listaB = Lista()
+listaB.agregar_final("a")
+listaB.agregar_final("t")
+listaB.agregar_final("B")
+listaB.agregar_final("x")
+
+listaA.mostrar_lista()
+listaB.mostrar_lista()
+
+comparar(listaA, listaB)
+
+lista_unida = juntar_listas(listaA, listaB)
+
+ordenar_lista(lista_unida)
+lista_unida.mostrar_lista()
+
+
--- lista_circular/LISTA_CIRCULAR.py
+++ lista_circular/LISTA_CIRCULAR.py
@@ -25,36 +25,137 @@ class ListaCircular():
             actual.siguiente = nuevo_nodo
             nuevo_nodo.siguiente = self.p
 
-    def mostrar_lista(self):
-        if self.vacia():
-            print("La lista esta vacia")
-            
-        else:
-            actual = self.p
-            print(actual.elemento, end=" -> ")
-            while actual.siguiente != self.p:
-                actual = actual.siguiente
-                print(actual.elemento , end=(" -> "))
-            print("ColaCircular")
-            
     # def mostrar_lista(self):
     #     if self.vacia():
     #         print("La lista esta vacia")
             
     #     else:
     #         actual = self.p
-            
+    #         print(actual.elemento, end=" -> ")
     #         while actual.siguiente != self.p:
-    #             print(actual.elemento, end=" -> ")
     #             actual = actual.siguiente
-    #         print(actual.elemento , end=(" -> ColaCircular"))
-    #         print()
+    #             print(actual.elemento , end=(" -> "))
+    #         print("ColaCircular")
+            
+    def mostrar_lista(self):
+        if self.vacia():
+            print("La lista esta vacia")
+            
+        else:
+            actual = self.p
+            while actual.siguiente != self.p:
+                print(actual.elemento, end=" -> ")
+                actual = actual.siguiente
+            print(actual.elemento , end=(" -> ColaCircular\n"))
+    
+    def agregar_inicio(self, valor):
+        nuevo_nodo = Nodo(valor)
+        if self.vacia():
+            self.p = nuevo_nodo
+            nuevo_nodo.siguiente = self.p
+            print("Se ha agregado al inicio:", valor)
+        
+        else:
+            actual = self.p
+            while actual.siguiente != self.p:
+                actual = actual.siguiente
+            actual.siguiente = nuevo_nodo
+            nuevo_nodo.siguiente = self.p
+            self.p = nuevo_nodo
+            print("Se ha agregado al inicio:", valor)
+            
+    def eliminar_inicio(self):
+        if self.vacia():
+            print("La lista esta vacia")
+            
+        else:
+            actual = self.p
+            if self.p == actual.siguiente:
+                self.p = None
+                print("Se ha eliminado:", actual.elemento)
+                
+            else:
+                while actual.siguiente != self.p:
+                    actual = actual.siguiente
+                print("Se ha eliminado del inicio:", self.p.elemento)
+                actual.siguiente = self.p.siguiente
+                self.p = actual.siguiente
+                                
+    def eliminar_final(self):
+        if self.vacia():
+            print("La lista esta vacia")
+        else:
+            actual = self.p
+            if self.p == actual.siguiente:
+                self.p = None
+                print("Se ha eliminado:", actual.elemento)
+                
+            else:
+                while actual.siguiente.siguiente != self.p:
+                    actual = actual.siguiente
+                print("Se ha eliminado del final:", actual.siguiente.elemento)
+                actual.siguiente = self.p
+    
+    def girar_lista(self):
+        self.p = self.p.siguiente
+            
+
+def eliminar_multiplos(lista):
+    if lista.vacia():
+        print("La lista está vacía")
+        return
+
+    actual = lista.p
+    inicio = lista.p
+    
+    while lista.p.elemento % 2 == 0 and actual.siguiente != lista.p:
+        print(f"Eliminando {lista.p.elemento} (múltiplo de 2, nodo cabeza)")
+        lista.eliminar_inicio()
+        actual = lista.p
+        inicio = lista.p
+
+    if lista.vacia():
+        return
+    
+    actual = lista.p
+    while actual.siguiente != inicio:
+        if actual.siguiente.elemento % 2 == 0:
+            print(f"Eliminando {actual.siguiente.elemento} (múltiplo de 2)")
+            actual.siguiente = actual.siguiente.siguiente 
+        else:
+            actual = actual.siguiente
+            
+                
+            
+# ----- P R U E B A S -------        
+
+# ListaNumerica1 = ListaCircular()
+# ListaNumerica1.agregar_inicio(3)
+# ListaNumerica1.agregar_inicio(9)
+# ListaNumerica1.agregar_inicio(12)
+# ListaNumerica1.agregar_inicio(6)
+# ListaNumerica1.agregar_inicio(30)
+# ListaNumerica1.agregar_final(21)
+# ListaNumerica1.agregar_inicio(27)
+# ListaNumerica1.agregar_final(24)
+# ListaNumerica1.agregar_inicio(18)
+# ListaNumerica1.mostrar_lista()
+# eliminar_multiplos(ListaNumerica1)
+# ListaNumerica1.mostrar_lista()
 
 
-# ----- P R U E B A S -------    
+
+
 ListaCircular1 = ListaCircular()
 ListaCircular1.agregar_final("Ana")
 ListaCircular1.agregar_final("Pedro")
 ListaCircular1.mostrar_lista()
-ListaCircular1.agregar_final("anthony")
+ListaCircular1.agregar_inicio("Anthony")
+ListaCircular1.mostrar_lista()
+ListaCircular1.mostrar_lista()
+ListaCircular1.agregar_inicio("James")
+ListaCircular1.agregar_final("Zara")
+ListaCircular1.mostrar_lista()
+ListaCircular1.eliminar_final()
+ListaCircular1.eliminar_inicio()
 ListaCircular1.mostrar_lista()
